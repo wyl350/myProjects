514785277e8da72d2a8612f5d17c523f6e883f6c
wyl350 <wyl350@126.com>
2020-04-14 15:26:17 +0800

    2020年4月14日

---
+++ crud-express-mongodb/student.js
@@ -0,0 +1,130 @@
+const fs = require('fs')
+const dbPath = './db.json'
+
+exports.find = function (callback) {
+  fs.readFile(dbPath, 'utf8', function (err, data) {
+    if (err) {
+      return callback(err)
+    }
+    callback(null, JSON.parse(data).students)
+  })
+}
+
+exports.findById = function (id, callback) {
+  fs.readFile(dbPath, 'utf8', function (err, data) {
+    if (err) {
+      return callback(err)
+    }
+    const students = JSON.parse(data).students
+    const ret = students.find(function (item) {
+      return item.id === parseInt(id)
+    })
+    callback(null, ret)
+  })
+}
+
+exports.save = function (student, callback) {
+  fs.readFile(dbPath, 'utf8', function (err, data) {
+    if (err) {
+      return callback(err)
+    }
+    const students = JSON.parse(data).students
+
+    // 添加 id ，唯一不重复
+    student.id = students[students.length - 1].id + 1
+
+    // 把用户传递的对象保存到数组中
+    students.push(student)
+
+    // 把对象数据转换为字符串
+    const fileData = JSON.stringify({
+      students: students
+    })
+
+    // 把字符串保存到文件中
+    fs.writeFile(dbPath, fileData, function (err) {
+      if (err) {
+        // 错误就是把错误对象传递给它
+        return callback(err)
+      }
+      // 成功就没错，所以错误对象是 null
+      callback(null)
+    })
+  })
+}
+
+exports.updateById = function (student, callback) {
+  fs.readFile(dbPath, 'utf8', function (err, data) {
+    if (err) {
+      return callback(err)
+    }
+    const students = JSON.parse(data).students
+
+    // 注意：这里记得把 id 统一转换为数字类型
+    student.id = parseInt(student.id)
+
+    // 你要修改谁，就需要把谁找出来
+    // EcmaScript 6 中的一个数组方法：find
+    // 需要接收一个函数作为参数
+    // 当某个遍历项符合 item.id === student.id 条件的时候，find 会终止遍历，同时返回遍历项
+    const stu = students.find(function (item) {
+      return item.id === student.id
+    })
+
+    // 这种方式你就写死了，有 100 个难道就写 100 次吗？
+    // stu.name = student.name
+    // stu.age = student.age
+
+    // 遍历拷贝对象
+    for (const key in student) {
+      stu[key] = student[key]
+    }
+
+    // 把对象数据转换为字符串
+    const fileData = JSON.stringify({
+      students: students
+    })
+
+    // 把字符串保存到文件中
+    fs.writeFile(dbPath, fileData, function (err) {
+      if (err) {
+        // 错误就是把错误对象传递给它
+        return callback(err)
+      }
+      // 成功就没错，所以错误对象是 null
+      callback(null)
+    })
+  })
+}
+
+exports.deleteById = function (id, callback) {
+  fs.readFile(dbPath, 'utf8', function (err, data) {
+    if (err) {
+      return callback(err)
+    }
+    const students = JSON.parse(data).students
+
+    // findIndex 方法专门用来根据条件查找元素的下标
+    const deleteId = students.findIndex(function (item) {
+      return item.id === parseInt(id)
+    })
+
+    // 根据下标从数组中删除对应的学生对象
+    students.splice(deleteId, 1)
+
+    // 把对象数据转换为字符串
+    const fileData = JSON.stringify({
+      students: students
+    })
+
+    // 把字符串保存到文件中
+    fs.writeFile(dbPath, fileData, function (err) {
+      if (err) {
+        // 错误就是把错误对象传递给它
+        return callback(err)
+      }
+      // 成功就没错，所以错误对象是 null
+      callback(null)
+    })
+  })
+}
---
+++ 前段渲染/README.md
@@ -0,0 +1,17 @@
+主要 参考 node 视频课件的 第五天 的讲解 promise 的那一部分。
+
+前后端渲染的区别：
+1. https://www.cnblogs.com/sugartang/p/12312702.html
+1. 
+前后端渲染系列文章三篇：
+1. https://www.manster.me/?p=473
+https://www.zhihu.com/question/28725977?answer_deleted_redirect=true
+1. 精读前后端渲染之争：https://blog.csdn.net/taoerchun/article/details/81836850
+1. 精度：https://github.com/camsong/blog/issues/8
+
+1. 前后端渲染对比的优缺点： https://blog.csdn.net/le_17_4_6/article/details/89717017?depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromBaidu-5&utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromBaidu-5
+
+
+大学四年自学走来，这些私藏的实用工具/学习网站我贡献出来了
+https://blog.csdn.net/m0_37907797/article/details/102781027?depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendHotData-39&utm_source=distribute.pc_relevant.none-task-blog-BlogCommendHotData-39
+
